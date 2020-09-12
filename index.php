9f8c0c7de3c02f7085f9c01a50a04a06b28d2468
priyesh040101 <priyeshyadav0215@gmail.com>
2020-09-12 19:31:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>sumpurna mart</title>
+</head>
+<body>
 <?php
 require_once ("include/initialize.php");
 // if(isset($_SESSION['IDNO'])){
@@ -86,3 +94,6 @@ require_once("theme/templates.php");
 
 ?>
 
+
+</body>
+</html>

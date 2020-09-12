8520329d399e1b6b78149d05b714aa3ac908dd77
priyesh040101 <priyeshyadav0215@gmail.com>
2020-09-12 19:54:16 +0530

    sss

---
+++ index.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Sumpurna</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img class="card-img-top" src="/imagesp/logo.jpg" alt="Card image cap" style="width:200px; hiegth:200px;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto ">
+                <li class="nav-item">
+                    <a class="nav-link" href="#partner">Our Partners</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">contact us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#order">order on phone</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#download">download our app</a>
+                </li>
+
+
+            </ul>
+
+        </div>
+    </nav>
+    <!-- NAVBAR ENDED -->
+    <!-- CARDS START -->
+    <br><br>
+    <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+        <h4 id="partner">Our partners</h4>
+        <div class="card-deck">
+            <div class="card">
+                <img class="card-img-top" src="/imagesp/global.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Global group of institution</h5>
+                    <p class="card-text"></p>
+                </div>
+                <div class="card-footer">
+
+                    <small class="text-muted">contact to our partner</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="/imagesp/central.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">central academy senior secondary school</h5>
+                    <p class="card-text"></p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">contact to our partner</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="/imagesp/radiance.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Radiance Coaching</h5>
+                    <p class="card-text"></p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">contact to our partner</small>
+                </div>
+            </div>
+        </div>
+        <!-- CARDS END -->
+
+        <!-- contact us started -->
+        <hr>
+        <br><br>
+        <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+            <h4 id="contact">Contact us</h4>
+            <form>
+
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4"> first name</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">last name</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                </div>
+
+
+                <div class="form-group col-md-4">
+                    <label for="inputState">Address within jabalpur</label>
+                    <select id="inputState" class="form-control">
+                  <option>garha</option>
+                  <option>damohnaka</option>
+                  <option>shivnagar</option>
+                  <option>vijay nagar</option>
+                  <option>bhedaghat</option>
+                </select>
+                </div>
+                <div class="form-group col-md-6">
+
+                    <textarea name="" id="" cols="30" rows="10">your suggestions...</textarea>
+                </div>
+
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+        <!-- ORDER ON PHONE STARTS -->
+        <br>
+        <hr>
+        <br><br>
+        <div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+            <h4 id="order"></h4>
+            <div class="card text-center">
+                <div class="card-header">
+                    ORDER BY CALL
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Sumpurna Mart</h5>
+                    <p class="card-text">+91 9516100732</p>
+                    <p class="card-text">+91 9516100732</p>
+                    <p class="card-text">+91 9516100732</p>
+
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
+            </div>
+
+
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
